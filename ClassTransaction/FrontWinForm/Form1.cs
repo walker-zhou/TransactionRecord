a05b4803381470ed36e04fa5955d2269eac074af
zhoujy <zhoujyq@qq.com>
2022-07-31 10:27:22 +0800

    验证WindowsFormApp 使用OpenAPI Client

---
+++ ClassTransaction/FrontWinForm/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+
+namespace FrontWinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            HttpClient httpClient = new HttpClient();
+            string baseUrl = "http://10.0.49.253:5270";
+            Client apiClient = new Client(baseUrl,httpClient);
+
+            var tempData=apiClient.StocksAllAsync().Result;
+
+
+            button1.Text = "loaded！";
+
+            dataGridView1.DataSource = tempData;
+
+        }
+    }
+}
